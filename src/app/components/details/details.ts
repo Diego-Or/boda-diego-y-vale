b77e7cb214a895362d85069d343ec0174760af4e
Diego Ortiz <dfortiz116@gmail.com>
2025-11-18 23:44:02 -0500

    location agregado a componentes

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -2,7 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-details',
-  templateUrl: 'details.html'
+  templateUrl: 'details.html',
+  styleUrl: 'details.scss'
 })
 
 export class detailsComponent {
---
+++ src/app/components/location/location.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-location',
+  templateUrl: 'location.html',
+  styleUrl: 'location.scss'
+})
+
+export class locationComponent {
+  constructor() { }
+
+  ngOnInit() { }
+}
