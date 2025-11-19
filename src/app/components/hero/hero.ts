bc247f8b7090354b1a50d9d5837ea87e3fba77e8
Diego Ortiz <dfortiz116@gmail.com>
2025-11-19 00:13:39 -0500

    Implementación del contador en hero.ts

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './hero.scss'
 })
 export class heroComponent {
+
+  days = signal(0);
+  hours = signal(0);
+  minutes = signal(0);
+  seconds = signal(0);
+  distance = signal(1);
+
+  constructor (){
+
+    const updateCountdown = () => {
+      const weddingDate = new Date('2026-04-15T17:00:00').getTime();
+      const now = new Date().getTime();
+      const distance = weddingDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // document.getElementById('days').textContent = days;
+
+      this.days.set(days);
+      this.hours.set(hours);
+      this.minutes.set(minutes);
+      this.seconds.set(seconds);
+      this.distance.set(distance);
+
+    }
+
+    setInterval(updateCountdown, 1000);
+    updateCountdown();
+
+  }
+
+
+
+
 
 }
