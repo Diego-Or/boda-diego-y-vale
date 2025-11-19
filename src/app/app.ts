cdb36f6059e5e6fb40bc9a155399b05a9c87b29f
Diego Ortiz <dfortiz116@gmail.com>
2025-11-18 23:52:28 -0500

    FAQs agregado a components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { faqComponent } from './components/faq/faq';
 import { galleryComponent } from './components/gallery/gallery';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -11,7 +12,7 @@ import { rsvpComponent } from './components/assistance/rsvp';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, headerComponent, heroComponent, historyComponent, detailsComponent, locationComponent, rsvpComponent, galleryComponent, footerComponent],
+  imports: [RouterOutlet, headerComponent, heroComponent, historyComponent, detailsComponent, locationComponent, rsvpComponent, galleryComponent, faqComponent,footerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/faq/faq.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-faq',
+  templateUrl: 'faq.html',
+  styleUrl: 'faq.scss'
+})
+
+export class faqComponent {
+  constructor() { }
+
+  ngOnInit() { }
+}
