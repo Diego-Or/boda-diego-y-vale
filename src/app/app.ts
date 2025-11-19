40303757a0fb34ea125684e31a9f73d63a01b61b
Diego Ortiz <dfortiz116@gmail.com>
2025-11-18 23:42:09 -0500

    details agregado a componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { headerComponent } from './shared/Nav/header';
 import { footerComponent } from './shared/footer/footer';
 import { heroComponent } from './components/hero/hero';
 import { historyComponent } from './components/history/history';
+import { detailsComponent } from './components/details/details';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, headerComponent, heroComponent, historyComponent, footerComponent],
+  imports: [RouterOutlet, headerComponent, heroComponent, historyComponent, detailsComponent, footerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/details/details.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: 'details.html'
+})
+
+export class detailsComponent {
+  constructor() { }
+
+  ngOnInit() { }
+}
