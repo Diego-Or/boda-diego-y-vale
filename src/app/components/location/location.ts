720d3c9648337bd081e030345e9efad92ade4212
Diego Ortiz <dfortiz116@gmail.com>
2025-12-30 13:03:40 -0500

    Actualización en la ubicación con todos los detalles

--- src/app/components/location/location.ts
+++ src/app/components/location/location.ts
@@ -16,8 +16,8 @@ interface LocationData {
 export class locationComponent {
   // Señal con los datos de ubicación
   locationData = signal<LocationData>({
-    mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1987.262400728208!2d-74.00498842487563!3d5.018265162316214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4071b91a4a14d9%3A0x859d753e42baeaee!2sSalario%20Restaurante%20Bar!5e0!3m2!1ses-419!2sco!4v1763671807404!5m2!1ses-419!2sco',
-    googleMapsLink: 'https://maps.app.goo.gl/vvx2d1DLHADMj89f8',
+    mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1405.1345070352172!2d-73.98834135302378!3d5.051685542262284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e406f7d8259e95d%3A0x16de28e1d93d737c!2sRestaurante%20Campestre%20-%20La%20Hacienda!5e0!3m2!1ses-419!2sco!4v1767117505880!5m2!1ses-419!2sco',
+    googleMapsLink: 'https://maps.app.goo.gl/wUEYqF9B1DhHYToQ8',
   });
 
   // Computed signal para la URL sanitizada del mapa
